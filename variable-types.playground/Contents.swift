453accaa7fc2a7a217cc0fc8a3c93f00bff7620b
V <v@Vs-MacBook-Pro.local>
2016-01-29 16:35:54 +0200

    added files for super cool

---
+++ variable-types.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var helloworld = "Hello, playground" //string
+//helloworld += " I'm so cool"
+helloworld = helloworld + " I'm so cool"
+
+var numA = 1.5 //double
+var numB: Float = 1.5 //float
+var numC = 3 //int
+var sum = 5 + 6
+var product = 5 * 5
+var divisor = 20 / 5
+var remainder = 22 % 5
+var sub = 44 - 22
+
+var crazyResult = sum * product + (divisor * remainder + sub)
+
+var newStr = "Hey " + "how are you?"
+var firstName = "Jon"
+var lastName = "Smith"
+
+var fullName = "\(firstName) \(lastName)"
+
+var price = "Price $\(numA)"
+
+price = "Banana"
+price = "1.0"
+
+
+let total = 5.1 * 6.8 //let is constant
+
+var jack = 0, jill = 1, bob = 2
+
+jill = 6
+
+let beaver = "beaver", steever = "steever", cleaver = "clever"
+
+var totalPrice: Double
+
+totalPrice = 5.5
+
+var sumStr: String = "This is a string"
+
